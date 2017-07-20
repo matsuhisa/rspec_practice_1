64ebeba61d828fac65e81f0dcff6c747a2a457ca
matsuhisa <matsuhisa+github@gmail.com>
2017-07-21 00:56:53 +0900

    Refactoring book

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,16 @@
 class Book
+  NORMAL = [ {title: '坊ちゃん'}, {title: '我輩は猫である'} ]
+  NEWCOMER = [ {title: '桐島、部活やめるってよ'}, {title: 'ゆるいカーブ'} ]
+
+  def initialize(normal: nil, newcomer: nil)
+    @bookshelf = if normal || newcomer
+      normal.to_a + newcomer.to_a
+    else
+      NORMAL + NEWCOMER
+    end
+  end
+
   def message
-  	'hello'
+    @bookshelf.sample
   end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,7 +1,20 @@
 require 'spec_helper'
 
 RSpec.describe Book do
-  it "message return hello" do
-    expect(Book.new.message).to eq "hello"
+  describe '#message' do
+    let(:normal){ [ { title: '季節のかたみ' }, { title: '台所のおと' } ] }
+    let(:newcomer){ [ { title: '何様' }, { title: 'ラジオラジオラジオ！' } ] }
+
+    context '本の指定なし' do
+      it { expect(Book.new.message[:title] ).to eq('坊ちゃん') | eq('我輩は猫である') | eq('桐島、部活やめるってよ') | eq('ゆるいカーブ') }
+    end
+
+    context '本の指定あり' do
+      it { expect(Book.new(normal: normal, newcomer: newcomer).message[:title] ).to eq('季節のかたみ') | eq('台所のおと') | eq('何様') | eq('ラジオラジオラジオ！') }
+    end
+
+    context '本の指定新刊のみあり' do
+      it { expect(Book.new(newcomer: newcomer).message[:title] ).to eq('何様') | eq('ラジオラジオラジオ！') }
+    end
   end
 end
